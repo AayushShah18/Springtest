0c8749484d70d87c0ee44c1059da16344b7db1d3
aayushshah <aayushshah182002@gmail.com>
2026-01-14 22:54:10 +0530

    initial commit 3

--- src/main/java/com/test/demo/controller/TestController.java
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -18,8 +18,8 @@ public class TestController {
     public String test2(){
         return "test2";
     }
-    @GetMapping("/test2")
-    public String test2(){
-        return "test2";
+    @GetMapping("/test3")
+    public String test3(){
+        return "test3";
     }
 }
